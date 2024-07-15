90fef3b905b340741fafe3b5a4dc637c6802ddbc
ckhcree <kbyo0324@gmail.com>
2024-07-15 16:51:50 +0900

    :bug: response 수정

--- src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/dto/v1/PairingResponse.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/dto/v1/PairingResponse.kt
@@ -3,9 +3,9 @@ package sparta.nbcamp.wachu.domain.pairing.dto.v1
 data class PairingResponse(
     val id: Long,
     val wineId: Long,
+    val memberId: Long,
     val title: String,
     val description: String,
     val imageUrl: String,
     val createdAt: String,
-    val createdBy: Long,
 )
