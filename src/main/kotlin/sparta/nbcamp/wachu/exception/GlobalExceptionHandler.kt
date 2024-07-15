331712c53bff9bc3ba4a531cefb90c86812cb42d
Jaewon Noh <opqw987@gmail.com>
2024-07-15 20:28:34 +0900

    :sparkles: Global error handler

---
+++ src/main/kotlin/sparta/nbcamp/wachu/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,37 @@
+package sparta.nbcamp.wachu.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import sparta.nbcamp.wachu.exception.dto.ErrorDto
+import kotlin.io.AccessDeniedException
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(ModelNotFoundException::class)
+    fun handleModelNotFoundException(e: ModelNotFoundException): ResponseEntity<ErrorDto> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorDto(e.message, "404"))
+    }
+
+    @ExceptionHandler(IllegalStateException::class)
+    fun handleIllegalStateException(e: IllegalStateException): ResponseEntity<ErrorDto> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorDto(e.message, "400"))
+    }
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorDto> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorDto(e.message, "400"))
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(e: RuntimeException): ResponseEntity<ErrorDto> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorDto(e.message, "500"))
+    }
+
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handleAccessDeniedException(e: AccessDeniedException): ResponseEntity<ErrorDto> {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ErrorDto(e.message, "403"))
+    }
+}
---
+++ src/main/kotlin/sparta/nbcamp/wachu/exception/dto/ErrorDto.kt
@@ -0,0 +1,6 @@
+package sparta.nbcamp.wachu.exception.dto
+
+data class ErrorDto(
+    val message: String?,
+    val errorCode: String
+)
