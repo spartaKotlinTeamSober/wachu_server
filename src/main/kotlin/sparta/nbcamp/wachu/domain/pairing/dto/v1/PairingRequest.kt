9c3b32a3d8f5c74f6ded38fa761f2eaa3da3f50f
ckhcree <kbyo0324@gmail.com>
2024-07-15 18:06:10 +0900

    :bug: 일단 받는 이미지 스트링처리

--- src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/dto/v1/PairingRequest.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/dto/v1/PairingRequest.kt
@@ -1,10 +1,8 @@
 package sparta.nbcamp.wachu.domain.pairing.dto.v1
 
-import java.io.File
-
 data class PairingRequest(
     val wineId: Long,
     val title: String,
     val description: String,
-    val image: File
+    val image: String,
 )
