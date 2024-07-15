97b3c9a69e688751e8fed8b8931f9b8f2fc5d79d
ckhcree <kbyo0324@gmail.com>
2024-07-15 16:55:51 +0900

    :bug: 어노테이션 추가

--- src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/repository/v1/PairingRepositoryImpl.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/repository/v1/PairingRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package sparta.nbcamp.wachu.domain.pairing.repository.v1
 
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
 import sparta.nbcamp.wachu.domain.pairing.model.v1.Pairing
 
+@Repository
 class PairingRepositoryImpl(
     val pairingJpaRepository: PairingJpaRepository,
 ) : PairingRepository {
