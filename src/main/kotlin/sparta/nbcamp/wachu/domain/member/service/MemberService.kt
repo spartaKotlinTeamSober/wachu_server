d74d7a9bb2e1492fc7df87a74c9f9c71db7f03db
HJP_Kotlin_2 <wnd2gudwh@naver.com>
2024-07-16 11:55:40 +0900

    :art: 로그인,회원가입 구현 (#27)
    * :art: 로그인,회원가입 구현
    
    * :recycle: dto에 변환 로직 추가 및 check함수 사용
    
    * :recycle: SignUpresponse에 companionObject 적용,
    
    ---------
    
    Co-authored-by: PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>

--- src/main/kotlin/sparta/nbcamp/wachu/domain/member/service/MemberService.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/member/service/MemberService.kt
@@ -2,9 +2,11 @@ package sparta.nbcamp.wachu.domain.member.service
 
 import sparta.nbcamp.wachu.domain.member.dto.LoginRequest
 import sparta.nbcamp.wachu.domain.member.dto.SignUpRequest
+import sparta.nbcamp.wachu.domain.member.dto.SignUpResponse
+import sparta.nbcamp.wachu.domain.member.dto.TokenResponse
 
 interface MemberService {
-    fun signup(request: SignUpRequest)
+    fun signup(request: SignUpRequest): SignUpResponse
 
-    fun login(request: LoginRequest)
+    fun login(request: LoginRequest): TokenResponse
 }
