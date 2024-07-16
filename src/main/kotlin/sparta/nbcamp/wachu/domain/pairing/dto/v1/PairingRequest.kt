ffd12f6609648e650d086c01fef0a1d76f321eb7
ckhcree <kbyo0324@gmail.com>
2024-07-16 16:50:01 +0900

    :sparkles: 페어링 디티오 변환펑션 추가

--- src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/dto/v1/PairingRequest.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/dto/v1/PairingRequest.kt
@@ -1,8 +1,22 @@
 package sparta.nbcamp.wachu.domain.pairing.dto.v1
 
+import sparta.nbcamp.wachu.domain.pairing.model.v1.Pairing
+
 data class PairingRequest(
     val wineId: Long,
     val title: String,
     val description: String,
-    val image: String,
-)
+    val photo: String,
+) {
+    companion object {
+        fun toPairing(memberId: Long, pairingRequest: PairingRequest): Pairing {
+            return Pairing(
+                wineId = pairingRequest.wineId,
+                memberId = memberId,
+                title = pairingRequest.title,
+                description = pairingRequest.description,
+                photoUrl = pairingRequest.photo,
+            )
+        }
+    }
+}
