be2275721dcb22264cb7f53da7a8ec252686d101
ckhcree <kbyo0324@gmail.com>
2024-07-17 18:31:53 +0900

    :sparkles: 리뷰 디티오 변환펑션 + 별점조건 추가

--- src/main/kotlin/sparta/nbcamp/wachu/domain/review/dto/v1/ReviewRequest.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/review/dto/v1/ReviewRequest.kt
@@ -1,9 +1,26 @@
 package sparta.nbcamp.wachu.domain.review.dto.v1
 
+import sparta.nbcamp.wachu.domain.review.model.v1.Review
+
 data class ReviewRequest(
     val wineId: Long,
     val title: String,
     val description: String,
     val score: Double,
-    val image: String,
-)
+) {
+    companion object {
+        fun toEntity(memberId: Long, reviewRequest: ReviewRequest): Review {
+            return Review(
+                wineId = reviewRequest.wineId,
+                memberId = memberId,
+                title = reviewRequest.title,
+                description = reviewRequest.description,
+                score = reviewRequest.score,
+            )
+        }
+    }
+
+    fun hasOKScore(score: Double): Boolean {
+        return this.score in 0.0..5.0 && this.score % 0.5 == 0.0
+    }
+}
--- src/main/kotlin/sparta/nbcamp/wachu/domain/review/dto/v1/ReviewResponse.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/review/dto/v1/ReviewResponse.kt
@@ -1,5 +1,7 @@
 package sparta.nbcamp.wachu.domain.review.dto.v1
 
+import sparta.nbcamp.wachu.domain.review.model.v1.Review
+
 data class ReviewResponse(
     val id: Long,
     val wineId: Long,
@@ -7,6 +9,19 @@ data class ReviewResponse(
     val title: String,
     val description: String,
     val score: Double,
-    val imageUrl: String,
     val createdAt: String,
-)
+) {
+    companion object {
+        fun from(review: Review): ReviewResponse {
+            return ReviewResponse(
+                id = review.id!!,
+                wineId = review.wineId,
+                memberId = review.memberId,
+                title = review.title,
+                description = review.description,
+                score = review.score,
+                createdAt = review.createdAt.toString(),
+            )
+        }
+    }
+}
