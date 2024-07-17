33b4a633cd9a71f6c71edde4afd0a98a91647e16
ckhcree <kbyo0324@gmail.com>
2024-07-17 18:33:17 +0900

    :sparkles: 리포지토리 및 서비스 로직 추가

--- src/main/kotlin/sparta/nbcamp/wachu/domain/review/service/v1/ReviewService.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/review/service/v1/ReviewService.kt
@@ -2,10 +2,11 @@ package sparta.nbcamp.wachu.domain.review.service.v1
 
 import sparta.nbcamp.wachu.domain.review.dto.v1.ReviewRequest
 import sparta.nbcamp.wachu.domain.review.dto.v1.ReviewResponse
+import sparta.nbcamp.wachu.infra.security.jwt.UserPrincipal
 
 interface ReviewService {
     fun getReviewList(): List<ReviewResponse>
     fun getReview(id: Long): ReviewResponse
-    fun createReview(reviewRequest: ReviewRequest): ReviewResponse
-    fun deleteReview(id: Long)
+    fun createReview(userPrincipal: UserPrincipal, reviewRequest: ReviewRequest): ReviewResponse
+    fun deleteReview(userPrincipal: UserPrincipal, id: Long)
 }
--- src/main/kotlin/sparta/nbcamp/wachu/domain/review/service/v1/ReviewServiceImpl.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/review/service/v1/ReviewServiceImpl.kt
@@ -2,26 +2,47 @@ package sparta.nbcamp.wachu.domain.review.service.v1
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import sparta.nbcamp.wachu.domain.member.repository.MemberRepository
 import sparta.nbcamp.wachu.domain.review.dto.v1.ReviewRequest
 import sparta.nbcamp.wachu.domain.review.dto.v1.ReviewResponse
+import sparta.nbcamp.wachu.domain.review.repository.v1.ReviewRepository
+import sparta.nbcamp.wachu.exception.ModelNotFoundException
+import sparta.nbcamp.wachu.infra.security.jwt.UserPrincipal
 
 @Service
-class ReviewServiceImpl : ReviewService {
+class ReviewServiceImpl(
+    private val memberRepository: MemberRepository,
+    private val reviewRepository: ReviewRepository,
+) : ReviewService {
     override fun getReviewList(): List<ReviewResponse> {
-        TODO()
+        val reviewList = reviewRepository.findAll()
+        return reviewList.map { ReviewResponse.from(it) }
     }
 
     @Transactional(readOnly = true)
     override fun getReview(id: Long): ReviewResponse {
-        TODO()
+        val review = reviewRepository.findById(id)
+            ?: throw ModelNotFoundException("Review", id)
+        return ReviewResponse.from(review)
     }
 
     @Transactional
-    override fun createReview(reviewRequest: ReviewRequest): ReviewResponse {
-        TODO()
+    override fun createReview(userPrincipal: UserPrincipal, reviewRequest: ReviewRequest): ReviewResponse {
+        val member = memberRepository.findById(userPrincipal.memberId)
+            ?: throw ModelNotFoundException("Member", userPrincipal.memberId)
+        val review = ReviewRequest.toEntity(member.id!!, reviewRequest)
+        return ReviewResponse.from(reviewRepository.save(review))
     }
 
-    override fun deleteReview(id: Long) {
-        TODO()
+    override fun deleteReview(userPrincipal: UserPrincipal, id: Long) {
+        val review = reviewRepository.findById(id)
+            ?: throw ModelNotFoundException("Review", id)
+        check(
+            review.hasPermission(
+                userPrincipal.memberId,
+                userPrincipal.role
+            )
+        ) { throw IllegalArgumentException("not your review") }
+        reviewRepository.delete(review)
     }
 }
