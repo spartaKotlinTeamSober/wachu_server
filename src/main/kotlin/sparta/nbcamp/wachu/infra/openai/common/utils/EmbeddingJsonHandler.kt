496a3400ca798dd27cb0229d53980022c445f610
Jaewon Noh <opqw987@gmail.com>
2024-07-20 23:11:20 +0900

    :sparkles: EmbeddingJsonHandler 추가

---
+++ src/main/kotlin/sparta/nbcamp/wachu/infra/openai/common/utils/EmbeddingJsonHandler.kt
@@ -0,0 +1,35 @@
+package sparta.nbcamp.wachu.infra.openai.common.utils
+
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.kotlinModule
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.io.File
+
+@Component
+class EmbeddingJsonHandler(
+    @Value("\${embedding.file.path}") private val filePath: String
+) {
+    private val mapper = jacksonObjectMapper().apply { registerModules(kotlinModule()) }
+    private val file = File(filePath)
+
+    init {
+        if (!file.exists()) {
+            file.createNewFile()
+            mapper.writeValue(file, emptyMap<String, List<Double>>())
+        }
+    }
+
+    fun readData(): Map<String, List<Double>> {
+        return if (file.length() == 0L) {
+            mapOf()
+        } else {
+            mapper.readValue(file, object : TypeReference<Map<String, List<Double>>>() {})
+        }
+    }
+
+    fun writeData(data: Map<String, List<Double>>) {
+        mapper.writeValue(file, data)
+    }
+}
