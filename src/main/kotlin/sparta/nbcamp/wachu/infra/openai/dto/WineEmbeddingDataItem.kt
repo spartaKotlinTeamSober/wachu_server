0656a1f03f472575b2602c731a44265fa25a6742
Jaewon Noh <opqw987@gmail.com>
2024-07-22 09:37:24 +0900

    :art: isAroma json ignore

--- src/main/kotlin/sparta/nbcamp/wachu/infra/openai/dto/WineEmbeddingDataItem.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/infra/openai/dto/WineEmbeddingDataItem.kt
@@ -1,9 +1,12 @@
 package sparta.nbcamp.wachu.infra.openai.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 data class WineEmbeddingDataItem(
     val property: String,
     val embedding: List<Double>
 ) {
+    @JsonIgnore
     fun isAroma(): Boolean {
         return property.contains(WineEmbeddingData.AROMA_PREFIX)
     }
