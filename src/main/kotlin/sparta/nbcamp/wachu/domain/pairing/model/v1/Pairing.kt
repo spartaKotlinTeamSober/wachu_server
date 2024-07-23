ec9637e52c4033c3d273b3a96a703b849fe975e3
ckhcree <kbyo0324@gmail.com>
2024-07-23 14:15:06 +0900

    :art: 페어링-와인 ManyToOne

--- src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/model/v1/Pairing.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/model/v1/Pairing.kt
@@ -2,16 +2,22 @@ package sparta.nbcamp.wachu.domain.pairing.model.v1
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import org.hibernate.annotations.CreationTimestamp
+import sparta.nbcamp.wachu.domain.wine.entity.Wine
 import java.time.LocalDateTime
 
 @Entity(name = "pairing")
 class Pairing(
-    @Column
-    val wineId: Long,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wine_id")
+    val wine: Wine,
 
     @Column
     val memberId: Long,
