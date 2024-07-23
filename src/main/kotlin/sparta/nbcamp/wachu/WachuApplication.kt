2595928c558fbdc3a4875a4cd9dffdb67cf02dcc
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-07-23 19:50:22 +0900

    :coffin: import문 삭제

--- src/main/kotlin/sparta/nbcamp/wachu/WachuApplication.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/WachuApplication.kt
@@ -2,8 +2,6 @@ package sparta.nbcamp.wachu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
-
 
 @SpringBootApplication
 class WachuApplication
