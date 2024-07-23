4f38027e601e0c4d9108bce8321eefd966dd76cf
ckhcree <kbyo0324@gmail.com>
2024-07-23 21:21:22 +0900

    :art: 페어링리스폰스 wineId: Long -> wine: WineResponse 로 수정

--- src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/dto/v1/PairingResponse.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/pairing/dto/v1/PairingResponse.kt
@@ -1,11 +1,12 @@
 package sparta.nbcamp.wachu.domain.pairing.dto.v1
 
 import sparta.nbcamp.wachu.domain.pairing.model.v1.Pairing
+import sparta.nbcamp.wachu.domain.wine.dto.WineResponse
 import java.time.LocalDateTime
 
 data class PairingResponse(
     val id: Long,
-    val wineId: Long,
+    val wine: WineResponse,
     val memberId: Long,
     val title: String,
     val description: String,
@@ -16,7 +17,7 @@ data class PairingResponse(
         fun from(pairing: Pairing): PairingResponse {
             return PairingResponse(
                 id = pairing.id!!,
-                wineId = pairing.wine.id,
+                wine = WineResponse.from(pairing.wine),
                 memberId = pairing.memberId,
                 title = pairing.title,
                 description = pairing.description,
