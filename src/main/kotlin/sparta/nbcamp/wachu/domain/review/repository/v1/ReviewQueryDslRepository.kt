57d2b737c4a29f2b111ff5cc255721fa235033f4
ckhcree <kbyo0324@gmail.com>
2024-07-24 14:40:36 +0900

    :art: 리뷰 페이지네이션/페치조인 1단계

--- src/main/kotlin/sparta/nbcamp/wachu/domain/review/repository/v1/ReviewQueryDslRepository.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/review/repository/v1/ReviewQueryDslRepository.kt
@@ -6,26 +6,27 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 import sparta.nbcamp.wachu.domain.review.model.v1.QReview
 import sparta.nbcamp.wachu.domain.review.model.v1.Review
+import sparta.nbcamp.wachu.domain.wine.entity.QWine
 import sparta.nbcamp.wachu.infra.querydsl.QueryDslSupport
 
 @Repository
 class ReviewQueryDslRepository : QueryDslSupport() {
     private val review = QReview.review
+    private val wine = QWine.wine
 
     fun findPage(pageable: Pageable): Page<Review> {
+        val content = queryFactory
+            .selectFrom(review)
+            .leftJoin(review.wine, wine).fetchJoin()
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .fetch()
+
         val total = queryFactory
             .select(review.count())
             .from(review)
-            .fetchOne()
-            ?: 0L
+            .fetchOne() ?: 0L
 
-        val query = queryFactory
-            .selectFrom(review)
-            .offset(pageable.offset)
-            .limit(pageable.pageSize.toLong())
-        val content = query.fetch()
-
-        val page = PageImpl(content, pageable, total)
-        return page
+        return PageImpl(content, pageable, total)
     }
 }
