8dc2965ed13257b7fcd2b21c228f8a217f0e3c45
ckhcree <kbyo0324@gmail.com>
2024-07-25 15:32:18 +0900

    :sparkles: 멤버 도메인에 메일인증 추가

--- src/main/kotlin/sparta/nbcamp/wachu/domain/member/controller/MemberController.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/member/controller/MemberController.kt
@@ -4,16 +4,32 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import sparta.nbcamp.wachu.domain.member.dto.*
+import sparta.nbcamp.wachu.domain.member.dto.LoginRequest
+import sparta.nbcamp.wachu.domain.member.dto.ProfileResponse
+import sparta.nbcamp.wachu.domain.member.dto.SignUpRequest
+import sparta.nbcamp.wachu.domain.member.dto.SignUpResponse
+import sparta.nbcamp.wachu.domain.member.dto.TokenResponse
+import sparta.nbcamp.wachu.domain.member.emailcode.dto.SendCodeRequest
+import sparta.nbcamp.wachu.domain.member.emailcode.service.CodeService
 import sparta.nbcamp.wachu.domain.member.service.MemberService
 import sparta.nbcamp.wachu.infra.security.jwt.UserPrincipal
 
 @RestController
 class MemberController(
     private val memberService: MemberService,
+    private val codeService: CodeService,
 ) {
+
+    @PostMapping("/auth/sign-up/email-validation")
+    fun sendValidationCode(@RequestBody request: SendCodeRequest): ResponseEntity<Any> {
+        return ResponseEntity.status(HttpStatus.OK).body(codeService.sendCode(request.email))
+    }
 
     @PostMapping("/auth/sign-up")
     fun signUp(@RequestBody request: SignUpRequest): ResponseEntity<SignUpResponse> {
--- src/main/kotlin/sparta/nbcamp/wachu/domain/member/service/MemberServiceImpl.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/member/service/MemberServiceImpl.kt
@@ -5,7 +5,13 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
-import sparta.nbcamp.wachu.domain.member.dto.*
+import sparta.nbcamp.wachu.domain.member.dto.LoginRequest
+import sparta.nbcamp.wachu.domain.member.dto.ProfileResponse
+import sparta.nbcamp.wachu.domain.member.dto.SignUpRequest
+import sparta.nbcamp.wachu.domain.member.dto.SignUpResponse
+import sparta.nbcamp.wachu.domain.member.dto.TokenResponse
+import sparta.nbcamp.wachu.domain.member.emailcode.dto.SendCodeRequest
+import sparta.nbcamp.wachu.domain.member.emailcode.service.CodeService
 import sparta.nbcamp.wachu.domain.member.entity.MemberRole
 import sparta.nbcamp.wachu.domain.member.repository.MemberRepository
 import sparta.nbcamp.wachu.exception.ModelNotFoundException
@@ -19,10 +25,17 @@ class MemberServiceImpl @Autowired constructor(
     private val memberRepository: MemberRepository,
     private val passwordEncoder: PasswordEncoder,
     private val jwtTokenManager: JwtTokenManager,
-    private val s3Service: S3Service
+    private val s3Service: S3Service,
+    private val codeService: CodeService,
 ) : MemberService {
 
+    override fun sendValidationCode(request: SendCodeRequest) {
+        check(!memberRepository.existsByEmail(request.email)) { "존재하는 이메일" }
+        codeService.sendCode(request.email)
+    }
+
     override fun signup(request: SignUpRequest): SignUpResponse {
+        check(codeService.checkCode(request.email, request.code)) { "인증코드가 맞지 않음" }
         check(!memberRepository.existsByEmail(request.email)) { "존재하는 이메일" }
         check(request.password == request.confirmPassword) { "처음에 설정한 비밀번호와 다름" }
         check(!memberRepository.existsByNickname(request.nickname)) { "이미 존재하는 닉네임" }
