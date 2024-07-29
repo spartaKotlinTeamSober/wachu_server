54a591cc765e6da2093ee92b73fdd14148771e50
ckhcree <kbyo0324@gmail.com>
2024-07-29 13:33:22 +0900

    :sparkles: 테스트용 페어링 리포지토리 펑션 추가

--- src/test/kotlin/sparta/nbcamp/wachu/domain/pairing/repository/PairingTestRepositoryImpl.kt
+++ src/test/kotlin/sparta/nbcamp/wachu/domain/pairing/repository/PairingTestRepositoryImpl.kt
@@ -3,13 +3,15 @@ package sparta.nbcamp.wachu.domain.pairing.repository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import sparta.nbcamp.wachu.domain.pairing.model.v1.Pairing
+import sparta.nbcamp.wachu.domain.pairing.repository.v1.PairingJpaRepository
 import sparta.nbcamp.wachu.domain.pairing.repository.v1.PairingRepository
 import java.util.logging.Level
 import java.util.logging.Logger
 
 class PairingTestRepositoryImpl(
     private val pairing: Pairing,
-    private val pairingPage: Page<Pairing>
+    private val pairingPage: Page<Pairing>,
+    private val pairingJpaRepository: PairingJpaRepository
 ) : PairingRepository {
     override fun findById(id: Long): Pairing? {
         if (id !in 1..10) return null
@@ -26,5 +28,9 @@ class PairingTestRepositoryImpl(
 
     override fun delete(pairing: Pairing) {
         Logger.getLogger("PairingTestRepositoryImpl").log(Level.INFO, "delete")
+    }
+
+    fun saveAllforTest(pairings: List<Pairing>): List<Pairing> {
+        return pairingJpaRepository.saveAll(pairings)
     }
 }
