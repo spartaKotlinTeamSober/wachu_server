a4c3f777ed1e22160b5beba7864d30343ec39527
npureaun <kjihun89@gmail.com>
2024-08-08 20:15:01 +0900

    :recycle:  jwtmanager @value주입 시점 변경

--- src/main/kotlin/sparta/nbcamp/wachu/infra/security/jwt/JwtTokenManager.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/infra/security/jwt/JwtTokenManager.kt
@@ -14,11 +14,10 @@ import java.time.Instant
 import java.util.Date
 
 @Component
-class JwtTokenManager {
-    @Value("\${auth.jwt.issuer}")
-    private lateinit var issuer: String
-    @Value("\${auth.jwt.secret}")
-    private lateinit var secret: String
+class JwtTokenManager(
+    @Value("\${auth.jwt.issuer}") private var issuer: String,
+    @Value("\${auth.jwt.secret}") private var secret: String,
+) {
 
     companion object {
         const val TOKEN_TYPE_KEY = "tokenType"
