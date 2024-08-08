2c2476f565d4921f0c3605270caa5139cf09bc6e
ckhcree <kbyo0324@gmail.com>
2024-08-08 17:18:38 +0900

    :white_check_mark: 테스트용 임베디드 h2 연결테스트코드

---
+++ src/test/kotlin/sparta/nbcamp/wachu/domain/testH2db/H2DatabaseConnectionTest.kt
@@ -0,0 +1,38 @@
+package sparta.nbcamp.wachu.domain.testH2db
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
+import sparta.nbcamp.wachu.domain.member.entity.Member
+import sparta.nbcamp.wachu.domain.member.entity.MemberRole
+import sparta.nbcamp.wachu.domain.member.repository.MemberJpaRepository
+
+@DataJpaTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = ["classpath:application-test.yml"])
+class H2DatabaseConnectionTest {
+
+    @Autowired
+    private lateinit var memberJpaRepository: MemberJpaRepository
+
+    @Test
+    fun `H2 데이터베이스 연결 확인`() {
+        val member = Member(
+            email = "test@test.com",
+            password = "password",
+            nickname = "testuser",
+            profileImageUrl = "profile.png",
+            memberRole = MemberRole.MEMBER
+        )
+
+        memberJpaRepository.save(member)
+
+        val foundMember = memberJpaRepository.findByIdOrNull(member.id)
+
+        member.email shouldBe foundMember?.email
+    }
+}
