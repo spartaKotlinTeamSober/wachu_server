35eb81dd3c2e858512fee573e1d911e705f767ef
ckhcree <kbyo0324@gmail.com>
2024-08-09 20:22:37 +0900

    :art: 회원가입 리스폰스에 이미지url 추가

--- src/main/kotlin/sparta/nbcamp/wachu/domain/member/dto/SignUpResponse.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/member/dto/SignUpResponse.kt
@@ -6,13 +6,15 @@ data class SignUpResponse(
     val id: Long,
     val email: String,
     val nickname: String,
+    val profileUrl: String?
 ) {
     companion object {
         fun from(entity: Member): SignUpResponse {
             return SignUpResponse(
                 id = entity.id!!,
                 email = entity.email!!,
-                nickname = entity.nickname!!
+                nickname = entity.nickname!!,
+                profileUrl = entity.profileImageUrl
             )
         }
     }
