9ae6ad3ab3039749d66a885ba1da0c1a12653d48
npureaun <kjihun89@gmail.com>
2024-08-13 16:49:45 +0900

    :sparkles: 주요 비변동 데이터 인메모리화를 통한 성능개선

--- src/main/kotlin/sparta/nbcamp/wachu/infra/batches/s3directory/S3DirectoryBatchJob.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/infra/batches/s3directory/S3DirectoryBatchJob.kt
@@ -1,7 +1,8 @@
 package sparta.nbcamp.wachu.infra.batches.s3directory
 
 import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.ApplicationListener
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import sparta.nbcamp.wachu.domain.wine.entity.WineType
@@ -12,9 +13,11 @@ import sparta.nbcamp.wachu.infra.aws.s3.S3Service
 class S3DirectoryBatchJob(
     private val s3Service: S3Service,
     private val inMemoryDirectory: InMemoryDirectory
-) : ApplicationListener<ApplicationReadyEvent> {
+) {
 
-    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+    @Async
+    @EventListener(ApplicationReadyEvent::class)
+    fun onApplicationEvent(event: ApplicationReadyEvent) {
         job()
     }
 
--- src/main/kotlin/sparta/nbcamp/wachu/infra/batches/winedata/WineDataBatchJob.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/infra/batches/winedata/WineDataBatchJob.kt
@@ -2,7 +2,7 @@ package sparta.nbcamp.wachu.infra.batches.winedata
 
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationEventPublisher
-import org.springframework.context.ApplicationListener
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import sparta.nbcamp.wachu.domain.wine.repository.WineRepository
@@ -12,10 +12,11 @@ class WineDataBatchJob(
     private val wineRepository: WineRepository,
     private val inMemoryCache: InMemoryCache,
     private val eventPublisher: ApplicationEventPublisher
-) : ApplicationListener<ApplicationReadyEvent> {
+) {
 
     @Async
-    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+    @EventListener(ApplicationReadyEvent::class)
+    fun onApplicationEvent(event: ApplicationReadyEvent) {
         val wines = wineRepository.findAll()
         inMemoryCache.loadWines(wines)
         eventPublisher.publishEvent(DataLoadCompleteEvent(this))
