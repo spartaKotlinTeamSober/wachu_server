4354670b07cc882b7ef3015f68f63b13ecb8f59d
ckhcree <kbyo0324@gmail.com>
2024-08-13 20:43:45 +0900

    :sparkles: 생성된 프리사인드 겟

--- src/main/kotlin/sparta/nbcamp/wachu/infra/media/MediaS3Service.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/infra/media/MediaS3Service.kt
@@ -1,9 +1,11 @@
 package sparta.nbcamp.wachu.infra.media
 
 import org.springframework.web.multipart.MultipartFile
+import java.net.URL
 
 interface MediaS3Service {
     fun upload(file: MultipartFile, filePath: String): String
     fun upload(fileList: List<MultipartFile>, filePath: String): List<String>
     fun getS3Image(filePath: String): MutableList<String>
+    fun getPresignedUrl(file: MultipartFile, filePath: String): URL
 }
--- src/main/kotlin/sparta/nbcamp/wachu/infra/media/MediaS3ServiceImpl.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/infra/media/MediaS3ServiceImpl.kt
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile
 import sparta.nbcamp.wachu.domain.wine.service.WineImageGetter
 import sparta.nbcamp.wachu.infra.aws.s3.S3Service
 import sparta.nbcamp.wachu.infra.tika.TikaUtil
+import java.net.URL
 
 @Service
 class MediaS3ServiceImpl(
@@ -34,6 +35,14 @@ class MediaS3ServiceImpl(
     override fun getS3Image(filePath: String): MutableList<String> {
         //경로 안에 모든 파일을 불러옴
         return s3Service.getImage(filePath).toMutableList()
+    }
+
+    override fun getPresignedUrl(file: MultipartFile, filePath: String): URL {
+        if (!tikaUtil.validateMediaFile(file)) {
+            throw IllegalArgumentException("Invalid file format")
+        }
+        logger.info("upload file: $filePath")
+        return s3Service.generatePresignedUrl(file, filePath)
     }
 
     @PostConstruct
