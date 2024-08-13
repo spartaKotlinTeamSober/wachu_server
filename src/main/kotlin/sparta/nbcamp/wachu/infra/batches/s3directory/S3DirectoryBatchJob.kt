cfd78c947e7cb1c90e9a86c6b5c1b4b26ddd04dd
npureaun <kjihun89@gmail.com>
2024-08-13 17:05:12 +0900

    :recycle: testcode 오류 수정

--- src/main/kotlin/sparta/nbcamp/wachu/infra/batches/s3directory/S3DirectoryBatchJob.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/infra/batches/s3directory/S3DirectoryBatchJob.kt
@@ -1,8 +1,8 @@
 package sparta.nbcamp.wachu.infra.batches.s3directory
 
 import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
-import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import sparta.nbcamp.wachu.domain.wine.entity.WineType
@@ -10,12 +10,12 @@ import sparta.nbcamp.wachu.infra.aws.s3.S3FilePath
 import sparta.nbcamp.wachu.infra.aws.s3.S3Service
 
 @Component
+@Profile("!test")
 class S3DirectoryBatchJob(
     private val s3Service: S3Service,
     private val inMemoryDirectory: InMemoryDirectory
 ) {
 
-    @Async
     @EventListener(ApplicationReadyEvent::class)
     fun onApplicationEvent(event: ApplicationReadyEvent) {
         job()
