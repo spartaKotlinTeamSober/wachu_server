37d2dfaa6e93ed2d59ebb28ee1c3cebca6620c80
ckhcree <167150086+ckhcree@users.noreply.github.com>
2024-08-13 19:12:13 +0900

    :sparkles: 회원정보 추가 (#103)
    * :art: 회원가입에 프로필이미지 추가
    
    * :art: 회원가입 리스폰스에 이미지url 추가
    
    * :art: 회원정보 변경 dto 작성
    
    * :art: 회원정보 변경 val -> var
    
    * :art: 회원정보 변경 초안
    
    * :art: 회원정보 변경 세부
    
    * :art: prefix 를 auth -> api/v1
    
    * :art: dto 이름 수정
    
    * :art: 로직 순서 수정
    
    * :art: Entity 직접참조 X -> 멤버함수 O
    
    * :art: 회원정보 이메일변경 서비스구조 수정
    
    * :twisted_rightwards_arrows: 충돌 해결
    
    * :art: 프로필이미지url 코드 보완

--- src/main/kotlin/sparta/nbcamp/wachu/domain/member/service/MemberService.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/member/service/MemberService.kt
@@ -3,6 +3,7 @@ package sparta.nbcamp.wachu.domain.member.service
 import org.springframework.web.multipart.MultipartFile
 import sparta.nbcamp.wachu.domain.member.dto.LoginRequest
 import sparta.nbcamp.wachu.domain.member.dto.ProfileResponse
+import sparta.nbcamp.wachu.domain.member.dto.ProfileUpdateRequest
 import sparta.nbcamp.wachu.domain.member.dto.SignUpRequest
 import sparta.nbcamp.wachu.domain.member.dto.TokenResponse
 import sparta.nbcamp.wachu.domain.member.emailcode.dto.SendCodeRequest
@@ -12,13 +13,21 @@ import sparta.nbcamp.wachu.infra.security.oauth.dto.OAuthResponse
 interface MemberService {
     fun sendValidationCode(request: SendCodeRequest)
 
-    fun signup(request: SignUpRequest): ProfileResponse
+    fun signup(request: SignUpRequest, multipartFile: MultipartFile?): ProfileResponse
 
     fun login(request: LoginRequest): TokenResponse
 
     fun uploadProfile(userPrincipal: UserPrincipal, multipartFile: MultipartFile): ProfileResponse
 
     fun getProfile(userPrincipal: UserPrincipal): ProfileResponse
+
+    fun sendVerificationCodeForEmailUpdate(userPrincipal: UserPrincipal, request: SendCodeRequest): ProfileResponse
+
+    fun updateProfile(
+        userPrincipal: UserPrincipal,
+        request: ProfileUpdateRequest,
+        multipartFile: MultipartFile?,
+    ): ProfileResponse
 
     fun socialLogin(request: OAuthResponse): TokenResponse
 
