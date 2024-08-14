86fa86293deb4eeb21b6142f58ad2c98da858450
Jaewon Noh <opqw987@gmail.com>
2024-08-14 20:24:39 +0900

    :sparkles: 와인 추천 request 수정

--- src/main/kotlin/sparta/nbcamp/wachu/domain/wine/dto/RecommendWineRequest.kt
+++ src/main/kotlin/sparta/nbcamp/wachu/domain/wine/dto/RecommendWineRequest.kt
@@ -2,10 +2,7 @@ package sparta.nbcamp.wachu.domain.wine.dto
 
 data class RecommendWineRequest(
     val preferWineId: Long,
-    // val wineType: String,
-    // val priceWeight: Float,
-    // val sweetnessWeight: Float,
-    // val acidityWeight: Float,
-    // val bodyWeight: Float,
-    // val tanninWeight: Float,
+    val priceWeight: Int,
+    val tastyWeight: Int,
+    val aromaWeight: Int,
 )
